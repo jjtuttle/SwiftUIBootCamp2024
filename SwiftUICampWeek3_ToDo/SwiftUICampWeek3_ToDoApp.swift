fd2f2e04ab8f2c28d1cd07c848a8e5ea4b356b5d
James Tuttle <james.j.tuttle@gmail.com>
2024-01-20 11:59:26 -0800

    Initial Commit

---
+++ SwiftUICampWeek3_ToDo/SwiftUICampWeek3_ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICampWeek3_ToDoApp.swift
+//  SwiftUICampWeek3_ToDo
+//
+//  Created by James Tuttle on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICampWeek3_ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
